73bb0d05dfe804e1f22316ee527ac2133b2baa78
artyomkorchagin <steamartem@gmail.com>
2025-11-03 14:54:27 +0300

    Added autocommit to kafka client

--- internal/infrastructure/kafka.go
+++ internal/infrastructure/kafka.go
@@ -1,6 +1,8 @@
 package infrastructure
 
 import (
+	"time"
+
 	"github.com/IBM/sarama"
 	"github.com/artyomkorchagin/first-task/internal/config"
 )
@@ -8,6 +10,8 @@ import (
 func ConnectConsumer(cfg config.KafkaConfig) (sarama.Consumer, error) {
 	config := sarama.NewConfig()
 	config.Consumer.Return.Errors = cfg.ReturnErr
+	config.Consumer.Offsets.AutoCommit.Enable = true
+	config.Consumer.Offsets.AutoCommit.Interval = 1 * time.Second
 
 	return sarama.NewConsumer(cfg.Brokers, config)
 }
